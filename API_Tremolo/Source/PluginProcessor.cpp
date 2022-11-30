f288f71fff5706b409e2136cb239379a8b7cc98b
Felicia Redelaar <fredelaar@gmail.com>
2022-11-30 16:09:03 +0100

    changing interface a bit, adding tremolo effect with multiple waveforms

--- API_Tremolo/Source/PluginProcessor.cpp
+++ API_Tremolo/Source/PluginProcessor.cpp
@@ -3,8 +3,8 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 #include "PluginParameter.h"
-
-//==============================================================================
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 API_TremoloAudioProcessor::API_TremoloAudioProcessor():
 #ifndef JucePlugin_PreferredChannelConfigurations
@@ -18,10 +18,9 @@ API_TremoloAudioProcessor::API_TremoloAudioProcessor():
                    ),
 #endif
     parameters (*this)
-    , parameter1 (parameters, "Parameter 1", "", 0.0f, 1.0f, 0.5f, [](float value){ return value * 127.0f; })
-    , parameter2 (parameters, "Parameter 2", "", 0.0f, 1.0f, 0.5f)
-    , parameter3 (parameters, "Parameter 3", false, [](float value){ return value * (-2.0f) + 1.0f; })
-    , parameter4 (parameters, "Parameter 4", {"Option A", "Option B"}, 1)
+    , paramDepth(parameters, "Depth", "", 0.0f, 1.0f, 0.5f)
+    , paramFrequency(parameters, "LFO Frequency", "Hz", 0.0f, 10.0f, 2.0f)
+    , paramWaveform(parameters, "LFO Waveform", waveformItemsUI, waveformSine)
 {
     parameters.valueTreeState.state = juce::ValueTree (juce::Identifier (getName().removeCharacters ("- ")));
 }
@@ -30,22 +29,24 @@ API_TremoloAudioProcessor::~API_TremoloAudioProcessor()
 {
 }
 
-//==============================================================================
 
 void API_TremoloAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
     const double smoothTime = 1e-3;
-    parameter1.reset (sampleRate, smoothTime);
-    parameter2.reset (sampleRate, smoothTime);
-    parameter3.reset (sampleRate, smoothTime);
-    parameter4.reset (sampleRate, smoothTime);
+    paramDepth.reset(sampleRate, smoothTime);
+    paramFrequency.reset(sampleRate, smoothTime);
+    paramWaveform.reset(sampleRate, smoothTime);
+
+    lfoPhase = 0.0f;
+    inverseSampleRate = 1.0f / (float)sampleRate;
+    twoPi = 2.0f * M_PI;
 }
 
 void API_TremoloAudioProcessor::releaseResources()
 {
 }
 
-void API_TremoloAudioProcessor::processBlock (juce::AudioSampleBuffer& buffer, juce::MidiBuffer& midiMessages)
+void API_TremoloAudioProcessor::processBlock(juce::AudioSampleBuffer& buffer, juce::MidiBuffer& midiMessages)
 {
     juce::ScopedNoDenormals noDenormals;
 
@@ -55,51 +56,85 @@ void API_TremoloAudioProcessor::processBlock (juce::AudioSampleBuffer& buffer, j
 
     //======================================
 
-    float currentParameter2 = parameter2.getNextValue();
-    float currentParameter3 = parameter3.getNextValue();
-    float currentParameter4 = parameter4.getNextValue();
+    float currentDepth = paramDepth.getNextValue();
+    float currentFrequency = paramFrequency.getNextValue();
+    float phase;
 
-    float factor = currentParameter2 * currentParameter3 * currentParameter4;
+
 
     for (int channel = 0; channel < numInputChannels; ++channel) {
-        float* channelData = buffer.getWritePointer (channel);
+        float* channelData = buffer.getWritePointer(channel);
+        phase = lfoPhase;
 
         for (int sample = 0; sample < numSamples; ++sample) {
             const float in = channelData[sample];
-            float out = in * factor;
+            float modulation = lfo(phase, (int)paramWaveform.getTargetValue());
+            float out = in * (1 - currentDepth + currentDepth * modulation);
 
             channelData[sample] = out;
+
+            phase += currentFrequency * inverseSampleRate;
+            if (phase >= 1.0f)
+                phase -= 1.0f;
         }
     }
 
-    for (int channel = numInputChannels; channel < numOutputChannels; ++channel)
-        buffer.clear (channel, 0, numSamples);
-
-    //======================================
-
-    juce::MidiBuffer processedMidi;
-    juce::MidiMessage message;
-    int time;
-
-    for (juce::MidiBuffer::Iterator iter (midiMessages); iter.getNextEvent (message, time);) {
-        if (message.isNoteOn()) {
-            juce::uint8 newVel = (juce::uint8)(parameter1.getTargetValue());
-            message = juce::MidiMessage::noteOn (message.getChannel(), message.getNoteNumber(), newVel);
-        }
-        processedMidi.addEvent (message, time);
-    }
-
-    midiMessages.swapWith (processedMidi);
+    lfoPhase = phase;
 }
 
-//==============================================================================
+float API_TremoloAudioProcessor::lfo(float phase, int waveform)
+{
+	float out = 0.0f;
+	switch (waveform) {
+	case waveformSine: {
+		out = 0.5f + 0.5f * sinf(twoPi * phase);
+		break;
+	}
+	case waveformTriangle: {
+		if (phase < 0.25f)
+			out = 0.5f + 2.0f * phase;
+		else if (phase < 0.75f)
+			out = 1.0f - 2.0f * (phase - 0.25f);
+		else
+			out = 2.0f * (phase - 0.75f);
+		break;
+	}
+	case waveformSawtooth: {
+		if (phase < 0.5f)
+			out = 0.5f + phase;
+		else
+			out = phase - 0.5f;
+		break;
+	}
+	case waveformInverseSawtooth: {
+		if (phase < 0.5f)
+			out = 0.5f - phase;
+		else
+			out = 1.5f - phase;
+		break;
+	}
+	case waveformSquare: {
+		if (phase < 0.5f)
+			out = 0.0f;
+		else
+			out = 1.0f;
+		break;
+	}
+	case waveformSquareSlopedEdges: {
+		if (phase < 0.48f)
+			out = 1.0f;
+		else if (phase < 0.5f)
+			out = 1.0f - 50.0f * (phase - 0.48f);
+		else if (phase < 0.98f)
+			out = 0.0f;
+		else
+			out = 50.0f * (phase - 0.98f);
+		break;
+	    }
+	}
+	return out;
+}
 
-
-
-
-
-
-//==============================================================================
 
 void API_TremoloAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
 {
@@ -115,8 +150,6 @@ void API_TremoloAudioProcessor::setStateInformation (const void* data, int sizeI
             parameters.valueTreeState.state = juce::ValueTree::fromXml (*xmlState);
 }
 
-//==============================================================================
-
 bool API_TremoloAudioProcessor::hasEditor() const
 {
     return true; // (change this to false if you choose to not supply an editor)
@@ -127,7 +160,6 @@ juce::AudioProcessorEditor* API_TremoloAudioProcessor::createEditor()
     return new API_API_TremoloAudioProcessorEditor (*this);
 }
 
-//==============================================================================
 
 #ifndef JucePlugin_PreferredChannelConfigurations
 bool API_TremoloAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
@@ -153,7 +185,6 @@ bool API_TremoloAudioProcessor::isBusesLayoutSupported (const BusesLayout& layou
 }
 #endif
 
-//==============================================================================
 
 const juce::String API_TremoloAudioProcessor::getName() const
 {
@@ -180,11 +211,11 @@ bool API_TremoloAudioProcessor::producesMidi() const
 
 bool API_TremoloAudioProcessor::isMidiEffect() const
 {
-   #if JucePlugin_IsMidiEffect
+#if JucePlugin_IsMidiEffect
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
 double API_TremoloAudioProcessor::getTailLengthSeconds() const
@@ -194,8 +225,8 @@ double API_TremoloAudioProcessor::getTailLengthSeconds() const
 
 int API_TremoloAudioProcessor::getNumPrograms()
 {
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
+    return 1;
+
 }
 
 int API_TremoloAudioProcessor::getCurrentProgram()
@@ -203,20 +234,20 @@ int API_TremoloAudioProcessor::getCurrentProgram()
     return 0;
 }
 
-void API_TremoloAudioProcessor::setCurrentProgram (int index)
+void API_TremoloAudioProcessor::setCurrentProgram(int index)
 {
 }
 
-const juce::String API_TremoloAudioProcessor::getProgramName (int index)
+const juce::String API_TremoloAudioProcessor::getProgramName(int index)
 {
     return {};
 }
 
-void API_TremoloAudioProcessor::changeProgramName (int index, const juce::String& newName)
+void API_TremoloAudioProcessor::changeProgramName(int index, const juce::String& newName)
 {
 }
 
-//==============================================================================
+
 
 // This creates new instances of the plugin..
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
@@ -224,4 +255,3 @@ juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
     return new API_TremoloAudioProcessor();
 }
 
-//==============================================================================
--- API_Tremolo/Source/PluginProcessor.h
+++ API_Tremolo/Source/PluginProcessor.h
@@ -10,41 +10,26 @@
 class API_TremoloAudioProcessor : public juce::AudioProcessor
 {
 public:
-    //==============================================================================
 
     API_TremoloAudioProcessor();
     ~API_TremoloAudioProcessor();
-
-    //==============================================================================
-
+     
     void prepareToPlay (double sampleRate, int samplesPerBlock) override;
     void releaseResources() override;
     void processBlock (juce::AudioSampleBuffer&, juce::MidiBuffer&) override;
 
-    //==============================================================================
-
-
-
-
-
-
-    //==============================================================================
-
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
-    //==============================================================================
 
     juce::AudioProcessorEditor* createEditor() override;
     bool hasEditor() const override;
 
-    //==============================================================================
 
    #ifndef JucePlugin_PreferredChannelConfigurations
     bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
    #endif
 
-    //==============================================================================
 
     const juce::String getName() const override;
 
@@ -53,7 +38,6 @@ public:
     bool isMidiEffect () const override;
     double getTailLengthSeconds() const override;
 
-    //==============================================================================
 
     int getNumPrograms() override;
     int getCurrentProgram() override;
@@ -61,24 +45,39 @@ public:
     const juce::String getProgramName (int index) override;
     void changeProgramName (int index, const juce::String& newName) override;
 
-    //==============================================================================
+    juce::StringArray waveformItemsUI = {
+        "Sine",
+        "Triangle",
+        "Sawtooth (rising)",
+        "Sawtooth (falling)",
+        "Square",
+        "Square with sloped edges"
+    };
 
+    enum waveformIndex {
+        waveformSine = 0,
+        waveformTriangle,
+        waveformSawtooth,
+        waveformInverseSawtooth,
+        waveformSquare,
+        waveformSquareSlopedEdges,
+    };
 
+    float lfoPhase;
+    float inverseSampleRate;
+    float twoPi;
 
-
-
-
-    //==============================================================================
+    float lfo(float phase, int waveform);
 
     PluginParametersManager parameters;
 
-    PluginParameterLinSlider parameter1;
-    PluginParameterLinSlider parameter2;
-    PluginParameterToggle parameter3;
-    PluginParameterComboBox parameter4;
+    PluginParameterLinSlider paramDepth;
+    PluginParameterLinSlider paramFrequency;
+    PluginParameterComboBox paramWaveform;
+
 
 private:
-    //==============================================================================
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (API_TremoloAudioProcessor)
 };
+
